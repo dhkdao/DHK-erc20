0705b1c0be2f5a8705b36cfded972a5960d2ab7c
Rick Mak <rick.mak@gmail.com>
2025-12-02 14:57:10 +0800

    Add create2 based ignitation deplpyment

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ERC20 token contract for DHK - dao
 
+Current deplpoyed contract address is `0x759938b0D935f6e3eF60b01E0981F271DEd7078E`
+
 ## Development
 
 ```shell
@@ -16,7 +18,7 @@ send the init eth to deployment wallet for testing:
 
 ```
 cast send 0x4e073a1751953377f9b350b35847dedc4af742ea \
-  --value 100000000000000000 \
+  --value 100000000000000000000 \
   --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80  \
   --rpc-url http://127.0.0.1:8545
 ```
@@ -24,5 +26,8 @@ cast send 0x4e073a1751953377f9b350b35847dedc4af742ea \
 ## Deploy
 
 ```
-npx hardhat ignition deploy ./ignition/modules/DHK.ts
+npx hardhat ignition deploy ./ignition/modules/DHK.ts \
+    --strategy create2 \
+    --parameters ignition/parameters.json \
+    --network anvil 
 ```
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,57 @@
 import "@nomicfoundation/hardhat-toolbox-viem";
+import "@openzeppelin/hardhat-upgrades";
+import "@nomicfoundation/hardhat-verify";
 
+import dotenv from "dotenv";
 import type { HardhatUserConfig } from "hardhat/config";
+dotenv.config({ path: process.env.DOTENV_CONFIG_PATH || ".env" });
+
+const alchemyApiKey = process.env.ALCHEMY_API_KEY;
+const etherscanApiKey = process.env.ETHERSCAN_API_KEY;
+let signerKey =
+  process.env.DEPLOY_WALLET_PRIVATE_KEY ||
+  "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+signerKey = `0x${signerKey}`;
+const create2Salt =
+  process.env.CREATE2_SALT ||
+  "0x0000000000000000000000000000000000000000000000000000000000000000";
+
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    anvil: {
+      url: "http://localhost:8545",
+      chainId: 31337,
+      accounts: [signerKey],
+      gasMultiplier: 2,
+    },
+    base: {
+      chainId: 8453,
+      url: `https://base-mainnet.g.alchemy.com/v2/${alchemyApiKey}`,
+      accounts: [signerKey],
+    },
+    baseSepolia: {
+      chainId: 84532,
+      url: `https://base-sepolia.g.alchemy.com/v2/${alchemyApiKey}`,
+      accounts: [signerKey],
+    },
+  },
+  etherscan: {
+    apiKey: etherscanApiKey,
+  },
+  sourcify: {
+    enabled: true,
+    apiUrl: "https://sourcify.dev/server",
+    browserUrl: "https://repo.sourcify.dev",
+  },
+  ignition: {
+    strategyConfig: {
+      create2: {
+        salt: create2Salt,
+      },
+    },
+  },
 };
 
 export default config;
